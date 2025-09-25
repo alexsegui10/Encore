51ec03ec9986454aa03db8c81a047a29a7fd0d0f
dasaga26 <dasaga26@gmail.com>
2025-09-25 19:30:39 +0200

    Creacion de modelo y servicio de eventos

---
+++ frontend/src/app/shared/card-event/card-event.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input, } from '@angular/core';
+// import { Event } from '../../core/models/event.model';
+
+@Component({
+  selector: 'app-card-event',
+  templateUrl: './card-event.component.html',
+  styleUrls: ['./card-event.component.css']
+})
+
+export class CardEventComponent {
+
+}
---
+++ frontend/src/app/shared/list-events/list-events.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'list-events',
+    templateUrl: './list-events.component.html',
+    styleUrls: ['./list-events.component.css']
+})
+export class ListEventsComponent {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
