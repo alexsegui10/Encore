4364915fc8bbf87a1a43af44d98265c6e5a7b957
dasaga26 <dasaga26@gmail.com>
2025-09-26 17:42:21 +0200

    Carousel fixed

--- booking_client/app/routes/carousel.routes.js
+++ booking_client/app/routes/carousel.routes.js
@@ -4,9 +4,10 @@ import * as carousel from '../controllers/carousel.controller.js';
 
 const api = Router();
 
-api.get('/carousel/categories', carousel.get_carousel_category);
-//GET EVENT DATA FOR CAROUSEL
-api.get('/carousel/evento/:slug', carousel.get_carousel_evento);
+// GET todas las categorías para el carousel del home
+api.get('/carousel', carousel.get_carousel_category);
 
+// GET datos de un evento específico para el carousel de detalles
+api.get('/carousel/:slug', carousel.get_carousel_evento);
 
 export default api;
