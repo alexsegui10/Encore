482af4cfc93e8777ebfde35caeb6bf67bb288bee
dasaga26 <dasaga26@gmail.com>
2025-09-26 16:51:04 +0200

    Carousel

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -11,10 +11,5 @@ import { CarouselComponent } from '../../shared/carrusel/carousel.component';
     imports: [CommonModule, RouterModule, CarouselModule, CarouselComponent]
 })
 export class HomeComponent {
-    images = [
-        { src: 'assets/img1.jpg', title: 'Imagen 1' },
-        { src: 'assets/img2.jpg', title: 'Imagen 2' },
-        { src: 'assets/img3.jpg', title: 'Imagen 3' },
-        { src: 'assets/img4.jpg', title: 'Imagen 4' }
-    ];
+
 }
