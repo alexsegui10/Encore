24eb1568e389fe272bc3d9a23123c9ce4625165d
alexsegui10 <alexsegui10@gmail.com>
2025-09-30 20:12:24 +0200

    Update

--- frontend/src/app/shared/categorias/categorias.component.ts
+++ frontend/src/app/shared/categorias/categorias.component.ts
@@ -5,15 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { Category } from '../../core/models/category.model';
 import { CategoryService } from '../../core/services/category.service';
 import { CardCategoryComponent } from '../card-category/card-category.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
 
 @Component({
   selector: 'list-category',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule, CardCategoryComponent],
+  imports: [CommonModule, FormsModule, HttpClientModule, CardCategoryComponent, InfiniteScrollModule],
   templateUrl: './categorias.component.html',
   styleUrls: ['./categorias.component.css']
 })
 export class ListCategoryComponent implements OnInit {
+  offset = 0;
+  limit = 4;
   categories: Category[] = [];
 
   constructor(private categoryService: CategoryService) {}
@@ -23,9 +27,23 @@ export class ListCategoryComponent implements OnInit {
   }
 
   loadCategories(): void {
-    this.categoryService.getAllCategories().subscribe({
-      next: (cats) => { this.categories = cats; },
+   const params = this.getRequestParams(this.offset, this.limit);
+    this.categoryService.getAllCategories(params).subscribe({
+      next: (cats) => {
+        this.categories = cats;
+        this.limit = this.limit + 4; },
       error: (err) => { console.error('Error loading categories:', err); }
     });
+  }
+    getRequestParams(offset: number,limit: number): any{
+    let params: any = {};
+
+    params[`offset`] = offset;
+    params[`limit`] = limit;
+
+    return params;
+  }
+    scroll(): void {
+    this.loadCategories();
   }
 }
