793f58f49e48ddba90ef6d57677ecf318e34bc60
dasaga26 <dasaga26@gmail.com>
2025-12-12 17:08:06 +0100

    Search fix

--- frontend/src/app/shared/search/search.component.ts
+++ frontend/src/app/shared/search/search.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, OnInit, Output, signal } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Filters } from '../../core/models/filters.model';
-import { EventService } from '../../core/services/event.service';
 import { RagService } from '../../core/services/rag.service';
 import { EventBusService } from '../../core/services/event-bus.service';
 import { Event } from '../../core/models/event.model';
@@ -23,14 +22,11 @@ export class SearchComponent implements OnInit {
   listEvents: Event[] = [];
   filters: Filters = new Filters();
   routeFilters!: string | null;
-  search: any;
 
-  useAI = signal(false);
   aiLoading = signal(false);
   aiError = signal<string | null>(null);
 
   constructor(
-    private EventService: EventService,
     private ragService: RagService,
     private eventBusService: EventBusService,
     private Router: Router,
@@ -47,55 +43,8 @@ export class SearchComponent implements OnInit {
     this.search_value = this.filters.name || undefined;
   }
 
-  toggleAI() {
-    this.useAI.set(!this.useAI());
-    this.aiError.set(null);
-
-    if (!this.useAI()) {
-      this.eventBusService.clearAIEvents();
-    }
-  }
-
-  public type_event(writtingValue: any): void {
-    if (this.useAI()) {
-      return;
-    }
-
-    this.eventBusService.clearAIEvents();
-
-    this.routeFilters = this.ActivatedRoute.snapshot.paramMap.get('filters');
-    this.search = writtingValue;
-    this.filters.name = writtingValue;
-
-    setTimeout(() => {
-      this.searchEvent.emit(this.filters);
-      this.Location.replaceState('/shop/' + btoa(JSON.stringify(this.filters)));
-
-      if (this.search.length != 0) {
-        this.getListEvents()
-      }
-    }, 150);
-    this.filters.name = this.search;
-    this.filters.offset = 0;
-  }
-
-  getListEvents() {
-    this.EventService.getEventByName(this.search).subscribe(
-      (data: any) => {
-        this.listEvents = data.events;
-      });
-  }
-
   public search_event(data: any): void {
-    if (this.useAI()) {
-      this.searchWithAI();
-    } else {
-      if (typeof data.search_value === 'string') {
-        this.filters.name = data.search_value;
-        this.filters.offset = 0;
-        this.Router.navigate(['/shop/' + btoa(JSON.stringify(this.filters))]);
-      }
-    }
+    this.searchWithAI();
   }
 
   searchWithAI() {
@@ -121,11 +70,7 @@ export class SearchComponent implements OnInit {
         this.aiLoading.set(false);
 
         if (err.status === 503) {
-          this.aiError.set('LM Studio no disponible. Usando búsqueda tradicional...');
-          setTimeout(() => {
-            this.useAI.set(false);
-            this.type_event(question);
-          }, 1500);
+          this.aiError.set('LM Studio no disponible');
         } else {
           this.aiError.set('Error en búsqueda IA');
         }
@@ -133,4 +78,3 @@ export class SearchComponent implements OnInit {
     });
   }
 }
-
